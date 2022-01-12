5fbed94a729fc503a6e6bba709260486ad43561d
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-12 13:58:48 -0800

    Calucalte discount, old project uploaded

--- discount.py
+++ discount.py
@@ -1,1 +1,12 @@
-#discoutn
+# prompts user to enter original price
+price = int(input("Enter the price:"));
+# prompts user to enter percentage discount
+discount = int(input ("Enter discount percentage:"));
+#inputs taken in and calculations are made below to get ne price 
+discount = discount/100
+dollars_off= price * discount
+new_price= price - dollars_off
+dollars_off = str(dollars_off)
+new_price= str(new_price)
+#informs user of the new price and the difference between original price and new price
+print ("The new price is $"+ new_price + ", it is $"+ dollars_off + " off original price.")
